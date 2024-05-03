13fc4edb2d2a46897030f9b07e628ec662a6e1a8
Andrew Provencal <andrew.provencal@self.inc>
2024-05-03 14:01:31 -0500

    wip

--- examples/functions/base64tar/main.tf
+++ examples/functions/base64tar/main.tf
@@ -1,6 +1,77 @@
 locals {
+  sources = [
+    {
+      filename = "example/Chart.yaml"
+      content = replace(yamlencode({
+        apiVersion = "v2"
+        name       = "example"
+        type       = "application"
+        version    = "0.0.2"
+      }), "\"", "")
+    },
+    {
+      filename = "example/templates/deployment.yaml"
+      content = replace(yamlencode({
+        apiVersion = "apps/v1"
+        kind       = "Deployment"
+        metadata = {
+          name = "example"
+        }
+        spec = {
+          replicas = 2
+          selector = {
+            matchLabels = {
+              app = "example"
+            }
+          }
+          templates = {
+            metadata = {
+              labels = {
+                app = "example"
+              }
+              spec = {
+                containers = [
+                  {
+                    name  = "nginx"
+                    image = "nginx:latest"
+                    ports = [
+                      {
+                        containerPort = 80
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }), "\"", "")
+    },
+    {
+      filename = "example/templates/services.yaml"
+      content = replace(yamlencode({
+        apiVersion = "v1"
+        kind       = "Service"
+        metadata = {
+          name = "example"
+        }
+        spec = {
+          selector = {
+            app = "example"
+          }
+          ports = [
+            {
+              port       = 80
+              targetPort = 80
+            }
+          ]
+        }
+      }), "\"", "")
+    }
+  ]
   base64tar = (
-    provider::functions::base64tar(var.sources)
+    provider::functions::base64tar(local.sources)
   )
-  //tar = base64decode(local.base64tar)
+  base64tar_decoded = base64decode(local.base64tar)
+  base64tar_gzip    = base64gzip(local.base64tar_decoded)
 }
--- examples/functions/base64tar/outputs.tf
+++ examples/functions/base64tar/outputs.tf
@@ -1,3 +1,11 @@
-output "tar" {
-  value       = local.base64tar
+output "base64tar" {
+  value = local.base64tar
+}
+
+output "base64tar_decoded" {
+  value = local.base64tar_decoded
+}
+
+output "base64tar_gzip" {
+  value = local.base64tar_gzip
 }
